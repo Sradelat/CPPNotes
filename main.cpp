53ebbcefd6c37d92e901b80ac05b77b93148effc
Sradelat <shawnradelat@gmail.com>
2023-08-31 19:06:50 -0400

    12.x.3 Bookmark

--- main.cpp
+++ main.cpp
@@ -1,52 +1,177 @@
 #include <iostream>  // angled brackets are external directories
 
 
-// 12.8.3
-//
-#include <vector>
-#include <array>
+// 12.x.3
+#include <iostream>
+#include <iterator>
 
-using Numbers = std::vector<int>;
-
-int getStart()
+// array is the array to search over.
+// target is the value we're trying to determine exists or not.
+// min is the index of the lower bounds of the array we're searching.
+// max is the index of the upper bounds of the array we're searching.
+// binarySearch() should return the index of the target element if the target is found, -1 otherwise
+int binarySearch(const int* array, int target, int min, int max)
 {
-	std::cout << "Start where? ";
-	int start{};
-	std::cin >> start;
-	return start;
-}
-
-int getLength()
-{
-	std::cout << "How many? ";
-	int nums{};
-	std::cin >> nums;
-	return nums;
+    while (true)
+    {
+        int middle{ (min + max) / 2 };
+        if (array[middle] == target)
+        {
+            return middle;
+        }
+        else if (middle == min)
+        {
+            return -1;
+        }
+        else if (array[middle] > target)
+        {
+            max = middle;
+            std::cout << max << " max is middle.";
+        }
+        else if (array[middle] < target)
+        {
+            min = middle;
+            std::cout << min << " min is middle.";
+        }
+    }
 }
 
 int main()
 {
-	int start{getStart()};
-	int nums{getLength()};
-	int multiple{2};
+    constexpr int array[]{ 3, 6, 8, 12, 14, 17, 20, 21, 26, 32, 36, 37, 42, 44, 48 };
 
-	std::cout << "I generated " << nums << " square numbers. Do you know what "
-		"each number is after multiplying it by " << multiple << "?\n";
+    // We're going to test a bunch of values to see if they produce the expected results
+    constexpr int numTestValues{ 9 };
+    // Here are the test values
+    constexpr int testValues[numTestValues]{ 0, 3, 12, 13, 22, 26, 43, 44, 49 };
+    // And here are the expected results for each value
+    int expectedValues[numTestValues]{ -1, 0, 3, -1, -1, 8, -1, 13, -1 };
 
-	Numbers v(static_cast<Numbers::size_type>(nums));
-	//arr.resize(std::size_t(nums));
+    // Loop through all of the test values
+    for (int count{ 0 }; count < numTestValues; ++count)
+    {
+        // See if our test value is in the array
+        int index{ binarySearch(array, testValues[count], 0, static_cast<int>(std::size(array)) - 1) };
+        // If it matches our expected value, then great!
+        if (index == expectedValues[count])
+            std::cout << "test value " << testValues[count] << " passed!\n";
+        else // otherwise, our binarySearch() function must be broken
+            std::cout << "test value " << testValues[count] << " failed.  There's something wrong with your code!\n";
+    }
 
-	for (int& number : v) // need ampersand otherwise you GET A COPY!!
-	{
-		number = ((start * start) * multiple);
-		++start;
-	}
-
-	for (int num : v)
-		std::cout << num << ' ';
-
-	return 0;
+    return 0;
 }
+
+// 12.x.1
+//
+//const double max(const double a, const double b);
+//
+//void swap(int& a, int& y);
+//
+//int& getLargestElement(int* array, int length);
+
+// 12.8.3
+//
+//#include <vector>
+//#include <array>
+//#include <algorithm>
+//#include <cmath>
+//
+//using Numbers = std::vector<int>;
+//
+//int getStart()
+//{
+//	std::cout << "Start where? ";
+//	int start{};
+//	std::cin >> start;
+//	return start;
+//}
+//
+//int getLength()
+//{
+//	std::cout << "How many? ";
+//	int nums{};
+//	std::cin >> nums;
+//	return nums;
+//}
+//
+//Numbers generateNums(int start, int length, int multiple)
+//{
+//	Numbers v(static_cast<Numbers::size_type>(length));
+//
+//	for (int& number : v) // need ampersand otherwise you GET A COPY!!
+//	{
+//		number = ((start * start) * multiple);
+//		++start;
+//	}
+//
+//	std::cout << "I generated " << length << " square numbers. Do you know what "
+//		"each number is after multiplying it by " << multiple << "?\n";
+//
+//	for (int num : v)
+//		std::cout << num << ' ';
+//	std::cout << '\n';
+//
+//	return v;
+//}
+//
+//int getGuess()
+//{
+//	int guess{};
+//	std::cout << "Guess: ";
+//	std::cin >> guess;
+//	std::cout << guess << '\n'; // debug
+//	return guess;
+//
+//}
+//
+//int main()
+//{
+//	int start{getStart()};
+//
+//	int length{getLength()};
+//
+//	int multiple{2};
+//
+//	Numbers v = { generateNums(start, length, multiple) };
+//
+//
+//	bool gameOver = false;
+//	while (!gameOver)
+//	{
+//		
+//		int guess{ getGuess() };
+//		auto found{ std::find(v.begin(), v.end(), guess) };
+//		std::cout << *found << "WAS FOUND\n";
+//		if (found == v.end())
+//		{
+//			std::cout << "WRONG! YOU LOSE.";
+//			gameOver = true;
+//			break;
+//		}
+//		else
+//		{
+//			v.erase(found);
+//			--length;
+//
+//			for (int num : v)
+//				std::cout << num << ' ';  //debug
+//			std::cout << '\n';
+//
+//			if (length == 0)
+//			{
+//				std::cout << "You found all the numbers! YOU WIN!\n";
+//				break;
+//			}
+//
+//			else
+//			{
+//				std::cout << "CORRECT! There are " << length << " more numbers.\n";
+//			}
+//		}
+//	}
+//	return 0;
+//}
 
 // 12.7.2
 //
